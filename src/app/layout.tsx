f3af997bc23f522e1554f9de295d171046297f96
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-07-29 13:19:14 +0700

    chore: Update metadata description for Surakiat blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,19 @@ import * as React from "react";
 import { Providers } from "./provider";
 import { Metadata } from "next";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Surakiat",
-  description: "Surakiat is a blog and personal website where JJ shares insights, stories, and information on various topics. Explore posts about web development, technology, and more.",
+  description:
+    "Surakiat is a blog and personal website where JJ shares insights, stories, and information on various topics. Explore posts about web development, technology, and more.",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -16,6 +25,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <body>
         <ThemeProvider>
           <Providers>{children}</Providers>
