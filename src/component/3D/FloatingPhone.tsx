f21631c3f327a03ec3c499555f1ea4152487b90f
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-07-29 18:16:59 +0700

    chore: Update FloatingPhone component with 3D animation and gradient background
    Add 3D animation and gradient background to the FloatingPhone component in order to enhance the visual appeal and user experience of the phone display.

---
+++ src/component/3D/FloatingPhone.tsx
@@ -0,0 +1,91 @@
+import { motion } from "framer-motion";
+import { FiBatteryCharging, FiWifi } from "react-icons/fi";
+import Image from "next/image";
+import { useTheme } from "@/contexts/ThemeContext";
+
+const Phone3D: React.FC = () => {
+  return (
+    <section className="grid place-content-center">
+      <FloatingPhone />
+    </section>
+  );
+};
+
+const FloatingPhone: React.FC = () => {
+  return (
+    <div
+      style={{
+        transformStyle: "preserve-3d",
+        transform: "rotateY(-30deg) rotateX(15deg)",
+      }}
+      className="rounded-[24px] bg-gradient-to-br from-[#4EDFE7] to-[#00597B]"
+    >
+      <motion.div
+        initial={{
+          transform: "translateZ(8px) translateY(-2px)",
+        }}
+        animate={{
+          transform: "translateZ(32px) translateY(-8px)",
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          duration: 2,
+          ease: "easeInOut",
+        }}
+        className="relative h-[480px] sm:h-96 sm:w-56 w-[260px] rounded-[24px] border-2 border-b-4 border-r-4 border-white border-l-neutral-200 border-t-neutral-200 bg-neutral-900 p-1 pl-[3px] pt-[3px]"
+      >
+        <HeaderBar />
+        <Screen />
+      </motion.div>
+    </div>
+  );
+};
+
+const HeaderBar: React.FC = () => {
+  return (
+    <>
+      <div className="absolute left-[50%] top-2.5 z-10 h-2 w-16 -translate-x-[50%] rounded-md bg-neutral-900"></div>
+      <div className="absolute right-3 top-2 z-10 flex gap-2">
+        <FiWifi className="text-neutral-600" />
+        <FiBatteryCharging className="text-neutral-600" />
+      </div>
+    </>
+  );
+};
+
+const Screen: React.FC = () => {
+  const { theme } = useTheme();
+  const logoSrc =
+    theme === "light" ? "/Surakiat-DarkBG.png" : "/Surakiat-WhiteBG.png";
+  const ColorClass =
+    theme === "light" ? "bg-zinc-50 text-zinc-950" : "bg-zinc-950 text-zinc-50";
+  return (
+    <div
+      className={`relative z-0 grid h-full w-full place-content-center overflow-hidden rounded-[20px] ${ColorClass}`}
+    >
+      <div className="flex flex-col gap-4 w-full justify-center items-center">
+        <p className={`text-center font-bold text-2xl sm:text-xl`}>
+          Contact me
+        </p>
+        <Image
+          src={logoSrc}
+          alt="Surakiat-Logo"
+          width="0"
+          height="0"
+          sizes="100vw"
+          loading="lazy"
+          className="object-cover w-[70px] h-[70px]"
+        />
+      </div>
+      <button
+        className={`absolute bottom-4 left-4 right-4 z-10 rounded-lg py-2 text-sm font-medium ${ColorClass}`}
+      >
+        Send Message
+      </button>
+      <div className="absolute -bottom-72 left-[50%] h-96 w-96 -translate-x-[50%] rounded-full bg-gradient-to-br from-[#4EDFE7] to-[#00597B]" />
+    </div>
+  );
+};
+
+export default Phone3D;
--- src/component/HeroComponent/ContactHero.tsx
+++ src/component/HeroComponent/ContactHero.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { useTheme } from "@/contexts/ThemeContext";
+import { Input, Textarea, Button } from "@nextui-org/react";
+import Phone3D from "../3D/FloatingPhone";
+import { useState, ChangeEvent } from "react";
 
-const Contact = () => {
+const Contact: React.FC = () => {
   const { theme } = useTheme();
   const bgColorClass =
     theme === "light" ? "bg-zinc-50 text-zinc-950" : "bg-zinc-950 text-zinc-50";
+
+  const [title, setTitle] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    setter: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setter(e.target.value);
+  };
 
   return (
     <section className={`${bgColorClass} py-12 sm:py-6`}>
@@ -18,6 +35,96 @@ const Contact = () => {
           collaborations. This contact form is for personal communication only
           and not associated with any company.
         </p>
+      </div>
+      <div className="flex sm:flex-col-reverse sm:items-center sm:pt-6 sm:gap-6 w-full gap-24 px-12 sm:px-6">
+        <div className="flex flex-col w-full gap-2">
+          <div className="flex flex-col w-full gap-1">
+            <h3 className="text-base font-medium">Subject of Contact</h3>
+            <Input
+              type="text"
+              label="Subject of Contact"
+              variant="flat"
+              placeholder="Enter your subject of contact"
+              value={title}
+              onChange={(e) => handleInputChange(e, setTitle)}
+            />
+          </div>
+          <div className="flex gap-4 w-full">
+            <div className="flex flex-col w-full gap-1">
+              <h3 className="text-base font-medium">Name</h3>
+              <Input
+                type="text"
+                label="Name"
+                variant="flat"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => handleInputChange(e, setName)}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1">
+              <h3 className="text-base font-medium">Username</h3>
+              <Input
+                type="text"
+                label="Username"
+                variant="flat"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => handleInputChange(e, setUsername)}
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex gap-4 w-full">
+            <div className="flex flex-col w-full gap-1">
+              <h3 className="text-base font-medium">Phone</h3>
+              <Input
+                type="text"
+                label="Phone"
+                variant="flat"
+                placeholder="Enter your phone"
+                value={phone}
+                onChange={(e) => handleInputChange(e, setPhone)}
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1">
+              <h3 className="text-base font-medium">Email</h3>
+              <Input
+                type="email"
+                label="Email"
+                variant="flat"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => handleInputChange(e, setEmail)}
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col w-full gap-1">
+            <h3 className="text-base font-medium">Message</h3>
+            <Textarea
+              type="text"
+              label="Message"
+              variant="flat"
+              placeholder="Please type your message here"
+              value={message}
+              onChange={(e) => handleInputChange(e, setMessage)}
+              className="w-full"
+            />
+          </div>
+          <div className="w-20 h-auto pt-2">
+            <Button
+              radius="md"
+              className="bg-gradient-to-br from-[#4EDFE7] to-[#00597B]"
+            >
+              Send Message
+            </Button>
+          </div>
+        </div>
+        <div className="flex w-fit justify-center">
+          <Phone3D />
+        </div>
       </div>
     </section>
   );
