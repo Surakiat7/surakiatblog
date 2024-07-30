9224a5465c72ff93bbe793a7b9ea77eae627449c
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-07-30 19:48:23 +0700

    state

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -12,7 +12,7 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setThemeState] = useState<Theme>("light");
+  const [theme, setThemeState] = useState<Theme>("dark");
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme") as Theme;
