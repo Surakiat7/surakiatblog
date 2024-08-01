1a51545e5202cdd0da58892eb91e352bcb6c9698
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-01 23:55:17 +0700

    script fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,18 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-site-verification" content="Ip9y8ba35nX-ze-tKhZawZZ68MPtGecVLGQ-AEijGZE" />
+        <meta
+          name="google-site-verification"
+          content="Ip9y8ba35nX-ze-tKhZawZZ68MPtGecVLGQ-AEijGZE"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <ThemeProvider>
+          <MobileScreenProvider>
+            <Providers>{children}</Providers>
+          </MobileScreenProvider>
+        </ThemeProvider>
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=G-S66GX9CHSJ`}
@@ -50,13 +60,6 @@ export default function RootLayout({
           `,
           }}
         />
-      </Head>
-      <body>
-        <ThemeProvider>
-          <MobileScreenProvider>
-            <Providers>{children}</Providers>
-          </MobileScreenProvider>
-        </ThemeProvider>
       </body>
     </html>
   );
