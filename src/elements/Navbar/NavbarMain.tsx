43b520c375a18a21674257644f316c873ac3109f
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-02 16:16:58 +0700

    update navbar

--- src/elements/Navbar/NavbarMain.tsx
+++ src/elements/Navbar/NavbarMain.tsx
@@ -138,7 +138,7 @@ const NavbarElement: React.FC<NavbarElementProps> = ({
             className="object-cover w-[40px] h-[40px] cursor-pointer"
             onClick={onLogoClick}
           />
-          <p className={`font-bold sm:hidden ${TitleLinearColor}`}>Surakiat</p>
+          <p className={`font-bold sm:hidden md:hidden ${TitleLinearColor}`}>Surakiat</p>
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent className="sm:hidden flex gap-4" justify="center">
