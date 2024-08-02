af7e5ee0581fd4b47dddf44767bc828df64ff17c
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-02 14:20:03 +0700

    test performance

--- src/component/HeroComponent/HeroComponent.tsx
+++ src/component/HeroComponent/HeroComponent.tsx
@@ -5,10 +5,19 @@ import { AnimationProps, motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 import { IoIosArrowDown } from "react-icons/io";
 import { useTheme } from "@/contexts/ThemeContext";
+import dynamic from "next/dynamic";
 
 interface DarkGridHeroElementProps {
   scrollToAbout: () => void;
 }
+
+const Beams = dynamic(() => import("@/component/HeroComponent/Beams"), {
+  ssr: false,
+});
+const GradientGrid = dynamic(
+  () => import("@/component/HeroComponent/GradientGrid"),
+  { ssr: false }
+);
 
 export const DarkGridHero: React.FC<DarkGridHeroElementProps> = ({
   scrollToAbout,
@@ -135,154 +144,8 @@ const SplashButton: React.FC<ButtonProps> = ({
   );
 };
 
-const Beams: React.FC = () => {
-  const { width } = useWindowSize();
-
-  const numColumns = width ? Math.floor(width / GRID_BOX_SIZE) : 0;
-
-  const placements = [
-    {
-      top: GRID_BOX_SIZE * 0,
-      left: Math.floor(numColumns * 0.05) * GRID_BOX_SIZE,
-      transition: {
-        duration: 3.5,
-        repeatDelay: 5,
-        delay: 2,
-      },
-    },
-    {
-      top: GRID_BOX_SIZE * 12,
-      left: Math.floor(numColumns * 0.15) * GRID_BOX_SIZE,
-      transition: {
-        duration: 3.5,
-        repeatDelay: 10,
-        delay: 4,
-      },
-    },
-    {
-      top: GRID_BOX_SIZE * 3,
-      left: Math.floor(numColumns * 0.25) * GRID_BOX_SIZE,
-    },
-    {
-      top: GRID_BOX_SIZE * 9,
-      left: Math.floor(numColumns * 0.75) * GRID_BOX_SIZE,
-      transition: {
-        duration: 2,
-        repeatDelay: 7.5,
-        delay: 3.5,
-      },
-    },
-    {
-      top: 0,
-      left: Math.floor(numColumns * 0.7) * GRID_BOX_SIZE,
-      transition: {
-        duration: 3,
-        repeatDelay: 2,
-        delay: 1,
-      },
-    },
-    {
-      top: GRID_BOX_SIZE * 2,
-      left: Math.floor(numColumns * 1) * GRID_BOX_SIZE - GRID_BOX_SIZE,
-      transition: {
-        duration: 5,
-        repeatDelay: 5,
-        delay: 5,
-      },
-    },
-  ];
-
-  return (
-    <>
-      {placements.map((p, i) => (
-        <Beam
-          key={i}
-          top={p.top}
-          left={p.left - BEAM_WIDTH_OFFSET}
-          transition={p.transition || {}}
-        />
-      ))}
-    </>
-  );
-};
-
-const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState<WindowSize>({
-    width: undefined,
-    height: undefined,
-  });
-
-  useEffect(() => {
-    const handleResize = () =>
-      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
-
-    window.addEventListener("resize", handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return windowSize;
-};
-
-const Beam: React.FC<BeamType> = ({ top, left, transition = {} }) => {
-  return (
-    <motion.div
-      initial={{ y: 0, opacity: 0 }}
-      animate={{ opacity: [0, 1, 0], y: 32 * 8 }}
-      transition={{
-        ease: "easeInOut",
-        duration: 3,
-        repeat: Infinity,
-        repeatDelay: 1,
-        ...transition,
-      }}
-      style={{ top, left }}
-      className="absolute z-10 h-[124px] sm:h-[64px] w-[2px] bg-gradient-to-b from-[#4EDFE7]/0 to-[#4EDFE7]"
-    />
-  );
-};
-
-const GradientGrid: React.FC<{
-  theme: "light" | "dark";
-  strokeColor: string;
-}> = ({ theme, strokeColor }) => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 2.5, ease: "easeInOut" }}
-      className="absolute inset-0 z-0"
-    >
-      <div
-        style={{
-          backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='72' height='72' viewBox='0 0 32 32'%3e%3cg fill='none' stroke='${strokeColor}' stroke-width='.5'%3e%3cpath d='M1 16h30M1 8h30M1 24h30M1 4h30M1 12h30M1 20h30M1 28h30M5 0v32M9 0v32M13 0v32M17 0v32M21 0v32M25 0v32M29 0v32M3 0v32M7 0v32M11 0v32M15 0v32M19 0v32M23 0v32M27 0v32'/%3e%3c/g%3e%3c/svg%3e")`,
-        }}
-        className="absolute inset-0 bg-center [mask-image:radial-gradient(ellipse,white,transparent)]"
-      />
-    </motion.div>
-  );
-};
-
-const GRID_BOX_SIZE = 64;
-const BEAM_WIDTH_OFFSET = 1;
-
-interface BeamType {
-  top: number;
-  left: number;
-  transition?: AnimationProps["transition"];
-}
-
 type ButtonProps = {
   children: ReactNode;
   className?: string;
   scrollToAbout: () => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
-
-interface WindowSize {
-  width: number | undefined;
-  height: number | undefined;
-}
