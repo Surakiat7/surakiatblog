b9179105359a93edc580ddec98acc8030d26396d
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-02 14:48:20 +0700

    test performance

--- src/elements/Navbar/NavbarMain.tsx
+++ src/elements/Navbar/NavbarMain.tsx
@@ -191,7 +191,7 @@ const NavbarElement: React.FC<NavbarElementProps> = ({
         <NavbarMenuItem>
           <Divider style={{ backgroundColor: dividerColor }} />
           <div className={`flex w-full sm:text-center justify-center py-3`}>
-            <p className={`text-xs ${textColorClass}`}>
+            <p className={`text-sm sm:text-xs ${textColorClass}`}>
               © Copyright 2024 Surakiat. All rights reserved.
             </p>
           </div>
