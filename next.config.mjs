289de5e5fa37ff936451b33be3ebeb8c3edea3a3
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-02 13:17:06 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 // Configure the bundle analyzer
 const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === 'true',
 });
 
 // Next.js configuration
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_ENV: "PRODUCTION",
-    NEXT_PUBLIC_GA_MEASUREMENT_ID: "G-S66GX9CHSJ",
+    NEXT_PUBLIC_ENV: 'PRODUCTION',
+    NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
   },
   images: {
     domains: [
-      "surakiat.dev",
+      'surakiat.dev',
     ],
   },
 };
