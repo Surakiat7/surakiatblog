47bb4482a44bd8f8f62a3fb34cdc6a57725e0b0e
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-08-11 01:06:03 +0700

    optimize

--- src/app/(routes)/blog/[id]/page.tsx
+++ src/app/(routes)/blog/[id]/page.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 const DEFAULT_OG_IMAGE = `${process.env.NEXT_PUBLIC_IMGIX_DOMAIN}/Logo-openGraph.avif`;
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.surakiat.dev';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://www.surakiat.dev";
 
 export async function generateMetadata(
   { params }: Props,
@@ -17,11 +17,15 @@ export async function generateMetadata(
   const id = parseInt(params.id);
   const post = getPostDataById(id);
 
-  const imageUrl = post?.imgUrl ? new URL(post.imgUrl, BASE_URL).toString() : DEFAULT_OG_IMAGE;
+  const imageUrl = post?.imgUrl
+    ? new URL(post.imgUrl, BASE_URL).toString()
+    : DEFAULT_OG_IMAGE;
   const canonicalUrl = `${BASE_URL}/blog/${params.id}`;
 
   const title = post ? `${post.title} | Surakiat` : "Blog Post | Surakiat";
-  const description = post?.description || "Visit my blog to discover tips, techniques, and various methods for frontend development!";
+  const description =
+    post?.description ||
+    "Visit my blog to discover tips, techniques, and various methods for frontend development!";
 
   return {
     title: title,
@@ -30,7 +34,7 @@ export async function generateMetadata(
       title: title,
       description: description,
       url: canonicalUrl,
-      siteName: 'Surakiat Blog',
+      siteName: "Surakiat Blog",
       images: [
         {
           url: imageUrl,
@@ -39,36 +43,36 @@ export async function generateMetadata(
           alt: post ? `${post.title} | Surakiat` : "Blog | Surakiat",
         },
       ],
-      locale: 'th_TH',
-      type: 'article',
+      locale: "th_TH",
+      type: "article",
     },
     twitter: {
-      card: 'summary_large_image',
-      site: 'surakiat.dev',
+      card: "summary_large_image",
+      site: "surakiat.dev",
       title: title,
       description: description,
-      creator: '@surakiat',
+      creator: "@surakiat",
       images: {
         url: imageUrl,
         alt: title,
-      }
+      },
     },
     alternates: {
       canonical: canonicalUrl,
     },
-    authors: [{ name: 'Surakiat', url: 'https://www.facebook.com/Surakiatz/' }],
+    authors: [{ name: "Surakiat", url: "https://www.facebook.com/Surakiatz/" }],
     other: {
-      'og:image:width': '1200',
-      'og:image:height': '630',
+      "og:image:width": "1200",
+      "og:image:height": "630",
       // Line specific
-      'line:card': 'summary_large_image',
-      'line:title': title,
-      'line:description': description,
-      'line:image': imageUrl,
+      "line:card": "summary_large_image",
+      "line:title": title,
+      "line:description": description,
+      "line:image": imageUrl,
       // LinkedIn specific
-      'linkedin:title': title,
-      'linkedin:description': description,
-      'linkedin:image': imageUrl,
+      "linkedin:title": title,
+      "linkedin:description": description,
+      "linkedin:image": imageUrl,
     },
   };
 }
